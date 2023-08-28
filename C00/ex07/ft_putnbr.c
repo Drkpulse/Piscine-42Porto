5ed2041ee3a05a8d5189216d163a5f5a1184b95a
Jose Maria Ferreira De Cabral Beirao <joseferr@c1r9s10.42porto.com>
2023-08-28 18:38:30 +0100

    Added C

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseferr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 18:28:24 by joseferr          #+#    #+#             */
+/*   Updated: 2023/08/28 18:35:07 by joseferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void        ft_putnbr(int nbr)
+{
+    if (nbr < 0)
+    {
+        ft_putchar('-');
+        ft_putnbr(-nbr);
+    }
+    else if (nbr > 9)
+    {
+        ft_putnbr(nbr / 10);
+        ft_putnbr(nbr % 10);
+    }
+    else
+    {
+        ft_putchar(nbr + '0');
+    }
+}
+
+int main()
+{
+	ft_putnbr(nbr);
+}
