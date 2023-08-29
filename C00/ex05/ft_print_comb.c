e345bcbc9cbf32df0ee5eaa3fd4edd58166b59b1
Jose Maria Ferreira De Cabral Beirao <joseferr@c1r3s5.42porto.com>
2023-08-29 15:57:35 +0100

    ex5_c00 done

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,29 +6,39 @@
 /*   By: joseferr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 17:41:25 by joseferr          #+#    #+#             */
-/*   Updated: 2023/08/28 17:59:41 by joseferr         ###   ########.fr       */
+/*   Updated: 2023/08/29 15:55:14 by joseferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
+void    ft_putchar(char i,char n,char x)
+{
+	write(1, &i, 1);
+	write(1, &n, 1);
+	write(1, &x, 1);
+	if (i < '7')
+	{
+		write(1, ", ", 1);
+	}
+}
+
+
 void ft_print_comb(void)
 {
-	int i;
-	int n;
-	int x;
+	char i;
+	char n;
+	char x;
 
-	i = 0;
-	n = i + 1;
-	x = n + 1;
+	i = '0';
 	while(i<='7')
 	{
+		n = i + 1;
 		while(n<='8')
 		{
+			x = n + 1;
 			while(x<='9')
 			{
-				write(1,'i', 1);
-				write(1,'n', 1);
-				write(1,'x', 1);
+				ft_putchar(i,n,x);
 				x++;
 			}
 			n++;
@@ -37,4 +47,4 @@ void ft_print_comb(void)
 	}
 }
 
-int main(){ ft_print_comb();}
+/* int main(){ ft_print_comb();} */
