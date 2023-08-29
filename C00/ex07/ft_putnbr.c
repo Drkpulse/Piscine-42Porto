36d8b5c543dc828be571f2e179490c3b05ceb2a5
Beirão <phsicopt@gmail.com>
2023-08-29 20:08:41 +0100

    Cleaned ex07

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -16,25 +16,31 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void        ft_putnbr(int nbr)
+void        ft_putnbr(int x)
 {
-    if (nbr < 0)
+    if (x < 0)
     {
         ft_putchar('-');
-        ft_putnbr(-nbr);
+        ft_putnbr(-x);
     }
-    else if (nbr > 9)
+    else if (x > 9)
     {
-        ft_putnbr(nbr / 10);
-        ft_putnbr(nbr % 10);
+        ft_putnbr(x / 10);
+        ft_putnbr(x % 10);
     }
     else
     {
-        ft_putchar(nbr + '0');
+        ft_putchar(x + '0');
     }
 }
 
+/*
 int main()
 {
+    int nbr;
+
+    nbr = 99;
+
 	ft_putnbr(nbr);
 }
+*/
