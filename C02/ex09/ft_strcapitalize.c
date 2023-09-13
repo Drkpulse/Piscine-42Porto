80b31cb3a45950f1338ddca22991540bb8e87271
Jose Beirao <joseferr@students.42porto.com>
2023-09-13 16:26:51 +0100

    Ex09 Fixed

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -9,41 +9,48 @@
 /*   Updated: 2023/09/11 23:46:35 by joseferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-void	functionsthefunction(char *str, int c)
+char	*ft_strlowcase(char *str)
 {
-	if (c == 0 && *(str + c) >= 'a' && *(str + c) <= 'z')
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		*(str + c) = *(str + c) - 32;
-	}
-	else if ((*(str + c) >= '!' && *(str + c) <= '/') || (*(str + c) >= '['
-			&& *(str + c) <= '`') || *(str + c) == ' ')
-	{
-		if (*(str + c + 1) >= 'a' && *(str + c + 1) <= 'z')
+		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			*(str + c + 1) = *(str + c + 1) - 32;
+			str[i] = str[i] + 32;
 		}
+		else
+		{
+			str[i] = str[i];
+		}
+		i++;
 	}
+	return (str);
 }
 
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
-	int	c;
 
 	i = 0;
-	while (*(str + i) != '\0')
+	str = ft_strlowcase(str);
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - 32;
+	else if (str[i] == '\0')
+		return (str);
+	while (str[i] != '\0')
 	{
-		if (*(str + i) >= 'A' && *(str + i) <= 'Z')
+		if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
+				(str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
+				(str[i - 1] >= '0' && str[i - 1] <= '9')))
 		{
-			*(str + i) = *(str + i) + 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
 		}
 		i++;
-	}
-	c = 0;
-	while (*(str + c) != '\0')
-	{
-		functionsthefunction(str, c);
-		c++;
 	}
 	return (str);
 }
